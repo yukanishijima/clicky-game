136a3de98319346ed9e3b2cbf9651a144c39b65b
Yuka Nishijima <48897211+yukanishijima@users.noreply.github.com>
2019-12-22 19:29:21 -0500

    Add attribute for vector illustrations

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -17,19 +17,23 @@ function Header(props) {
       <div className="modal fade" id="instructions" tabIndex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
         <div className="modal-dialog modal-dialog-centered" role="document">
           <div className="modal-content">
+
             <div className="modal-header">
               <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true" className="close-btn">&times;</span>
               </button>
             </div>
+
             <div className="modal-body">
               <p><i className="fas fa-skull"></i>&nbsp; Click each image only once and never repeat!</p>
               <p><i className="fas fa-skull"></i>&nbsp; Be careful, every time any image is clicked, all of the images shuffle in a random order.</p>
               <p><i className="fas fa-skull"></i>&nbsp; You will loose when you click on the same image more than once!</p>
             </div>
-            <div className="modal-footer">
+            <div className="modal-footer" id="my-modal-footer">
+              <p id="special-thanks"><a href="https://www.freepik.com/katemangostar" target="_blank" rel="noopener noreferrer">Halloween vector illustrations created by katemangostar.</a></p>
               <button type="button" className="btn got-btn" data-dismiss="modal">Got it?</button>
             </div>
+
           </div>
         </div>
       </div>
